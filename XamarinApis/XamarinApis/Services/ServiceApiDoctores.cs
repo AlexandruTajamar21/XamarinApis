42ab1e1500f0bf42af1f94f95e73eeb57a1bebad
AlexandruTajamar21 <alexandru.preutesei@tajamar365.com>
2022-06-01 11:44:41 +0200

    Add project files.

---
+++ XamarinApis/XamarinApis/Services/ServiceApiDoctores.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using XamarinApis.Models;
+using System.Net.Http;
+
+namespace XamarinApis.Services
+{
+    public class ServiceApiDoctores
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceApiDoctores()
+        {
+            this.UrlApi = "https://apicruddoctores.azurewebsites.net/";
+            this.Header =
+                new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Doctor>> GetDoctoressAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/doctores";
+                List<Doctor> doctors = await this.CallApiAsync<List<Doctor>>(request);
+                return doctors;
+            }
+        }
+
+        public async Task<Doctor> FindDoctorAsync(int id)
+        {
+            string request = "/api/doctores/" + id;
+            Doctor doctor = await this.CallApiAsync<Doctor>(request);
+            return doctor;
+        }
+
+        public async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri uri = new Uri(this.UrlApi + request);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response =
+                    await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data =
+                        await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+    }
+}
---
+++ XamarinApis/XamarinApis/ViewModels/DoctoresListViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using XamarinApis.Base;
+using XamarinApis.Models;
+using XamarinApis.Services;
+using XamarinApis.Views;
+
+namespace XamarinApis.ViewModels
+{
+    public class DoctoresListViewModel: ViewModelBase
+    {
+        private ServiceApiDoctores service;
+
+        public DoctoresListViewModel(ServiceApiDoctores service)
+        {
+            this.service = service;
+        }
+
+        private ObservableCollection<Doctor> _Doctores;
+
+        public ObservableCollection<Doctor> Doctores
+        {
+            get { return this._Doctores; }
+            set
+            {
+                this._Doctores = value;
+                OnPropertyChanged("Doctores");
+            }
+        }
+
+        public Command MostrarDoctores
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    List<Doctor> data =
+                    await this.service.GetDoctoressAsync();
+                    this.Doctores =
+                    new ObservableCollection<Doctor>(data);
+                });
+            }
+        }
+
+        public Command ShowDoctor
+        {
+            get
+            {
+                return new Command(async(idDoctor) =>
+                {
+                    Doctor doctor = await this.service.FindDoctorAsync((int)idDoctor);
+                    DoctorDetailsView view = new DoctorDetailsView();
+                    DoctorDetailsViewModel viewmodel = new DoctorDetailsViewModel();
+                    viewmodel.Doctor = doctor;
+                    view.BindingContext = viewmodel;
+                    await Application.Current.MainPage.Navigation.PushModalAsync(view);
+                });
+            }
+        }
+    }
+}
